1cbc2c4532f073fc24380b30c8c0636a22ecfab9
Andrepc23 <andreraphaelcouto@gmail.com>
2022-11-12 12:23:53 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+ 
+export const options: Partial<null|IConfig> | (() => Partial<IConfig>) = null;
+ 
 import { AppComponent } from './app.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    FilterPipeModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
